df9f6cb78e486bc4a4785cda63812e4ea08d2568
Murilo Carvalho <murilocarvalhoweb@gmail.com>
2023-04-18 13:30:51 -0300

    Número de telefone da URL Final alterado

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ function generateLink(e) {
   let assunto = document.zapform.assunto.value;
   let mensagem = document.zapform.mensagem.value;
   let url = 'https://wa.me/';
-  let urlFinal = `${url}${5522988736353}?text=Você tem uma nova mensagem!%0a%0a*Nome:* ${nome}%0a*E-mail:* ${email}%0a*Assunto:* ${assunto}%0a%0a*Mensagem:* ${mensagem}`;
+  let urlFinal = `${url}${5522981573737}?text=Você tem uma nova mensagem!%0a%0a*Nome:* ${nome}%0a*E-mail:* ${email}%0a*Assunto:* ${assunto}%0a%0a*Mensagem:* ${mensagem}`;
 
   if (!nome || !email || !assunto || !mensagem) {
     Swal.fire('Falha ao enviar', 'Por favor, preencha todos os campos.', 'error');
