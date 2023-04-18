e29326570ba7c8b76a5387f4303c02d76e5ecfe2
Murilo Carvalho <murilocarvalhoweb@gmail.com>
2023-04-18 15:14:42 -0300

    Adiciona campos data para configurar o número de envio e alterações no JS

--- js/script.js
+++ js/script.js
@@ -1,24 +1,29 @@
 function generateLink(e) {
   e.preventDefault();
 
-  let nome = document.zapform.nome.value;
-  let email = document.zapform.email.value;
-  let assunto = document.zapform.assunto.value;
-  let mensagem = document.zapform.mensagem.value;
-  let url = 'https://wa.me/';
-  let urlFinal = `${url}${5522981573737}?text=Você tem uma nova mensagem!%0a%0a*Nome:* ${nome}%0a*E-mail:* ${email}%0a*Assunto:* ${assunto}%0a%0a*Mensagem:* ${mensagem}`;
+  const name = document.zapform.name.value;
+  const email = document.zapform.email.value;
+  const subject = document.zapform.subject.value;
+  const message = document.zapform.mensagem.value;
+  const countryCode = +document.zapform.getAttribute('data-country-code');
+  const phoneNumber = +document.zapform.getAttribute('data-phone-number');
+  const url = 'https://wa.me/';
+  const finalUrl = `${url}${+countryCode}${+phoneNumber}?text=Você tem uma nova mensagem!%0a%0a*Nome:* ${name}%0a*E-mail:* ${email}%0a*Assunto:* ${subject}%0a%0a*Mensagem:* ${message}`;
 
-  if (!nome || !email || !assunto || !mensagem) {
+  if (isNaN(countryCode) || isNaN(phoneNumber)) {
+    Swal.fire('Falha ao enviar', 'O número de telefone para o envio não foi cadastrado ou está incorreto.', 'error');
+    return;
+  } else if (!name || !email || !subject || !message) {
     Swal.fire('Falha ao enviar', 'Por favor, preencha todos os campos.', 'error');
   } else {
-    enviarLink(urlFinal);
+    send(finalUrl);
   }
 }
 
-function enviarLink(urlFinal) {
+function send(finalUrl) {
   Swal.fire('Mensagem enviada!', 'Obrigado pelo seu contato.', 'success');
 
-  window.open(urlFinal);
+  window.open(finalUrl);
 
   document.zapform.reset();
 }
